974aa71e7190c4d5a7f00221a22e3ad721e5c9d3
light <agy91521947@gmail.com>
2024-08-29 15:42:11 +0900

    combination-binominal coefficient- calculation algorithm

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,21 +1,26 @@
 import sys
-def main():
-    input = sys.stdin.read
-    data = input().strip().split()
-    N = int(data[0])
-    
-    card = deque(data[1:])
-    card_dequeue = []
-    ind = 0
-    while True:
-        if not len(card):
-            break
-        card_dequeue.append(card.pop([ind]))
-        ind += card_dequeue[-1] 
+def combi(n, r):
+    """Calculating the binominal coefficient when in n, r"""
+    np = 1
+    rp = 1
+    for i in range(r):
+        np *= (n-i)
+        rp *= (i+1)
+    result = np//rp
+    return result  
 
-    sys.stdout.write(" ".join(card_dequeue) + " ")
+def main():
+    N = int(input())
+    out = []
+    for i in range(N):
+        s,n = map(int, input().split())
+        if s > n-s :
+            out.append(combi(n,n-s))
+        else:
+            out.append(combi(n, s))
+            
+    for i in range(N):
+        print(out[i])
 
 if __name__ == "__main__":
     main()
-
-
