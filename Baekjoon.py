787d47fa2b617a208ecd4ea958578f6c2beaa66d
yoonkeen <agy91521947@gmail.com>
2024-11-14 21:15:04 +0900

    1018: 체스판 다시 칠하기

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,22 +1,8 @@
-def fibo_recursion(number: int) -> int:
-    """
-    fibonacci function by recursion.
-    :param number: integer number
-    :return: integer number
-    """
-    if number == 0:
-        return 0
-    elif number == 1:
-        return 1
-    else:
-        return fibo_recursion(number - 1) + fibo_recursion(number - 2)
-
-
 
 
 def main():
-    N = int(input())  # 카드의 개수 입력 받기
-    print(fibo_recursion(N))
+    n,m = map(int, input().split())
+    
 
 
 if __name__ == "__main__":
