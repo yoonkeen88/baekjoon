711982547963c84c2f67efce0f693ba6e3b61378
yoonkeen <agy91521947@gmail.com>
2024-11-23 19:27:49 +0900

    4153번 직각삼각형

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,4 +1,27 @@
+import sys
+
 def main():
+    answer = []
+    while True:
+        try:
+            a = list(map(int, input().split()))
+        except ValueError:
+            print("숫자 세 개를 입력하세요.")
+            continue
+        if a == [0, 0, 0]: 
+            break
+        
+        if len(a) != 3:  
+            print("정확히 세 개의 숫자를 입력하세요.")
+            continue
+        
+        a.sort()
+        if a[0]**2 + a[1]**2 == a[2]**2:
+            answer.append("right")
+        else:
+            answer.append("wrong")
+
+    sys.stdout.write("\n".join(answer) + "\n")
 
 if __name__ == "__main__":
     main()
