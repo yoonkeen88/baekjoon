445b96464badbda2c205e5a8e3807faf5bfcc6a4
light <agy91521947@gmail.com>
2025-01-05 17:39:29 +0900

    10814 sorting by age & enter seq

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,16 +1,20 @@
-def main():
-    a,b = map(int, input().split())
 
-    if a < b:
-        a,b = b,a
-    mx = 1
-    for i in range(b,0,-1):
-        if a%i ==0 and b%i == 0:
-            mx =i
-            break
-        else: 
-            continue
-    print(f"{mx}\n{a*b//mx}")
+
+def main():
+    n = int(input())  # Number of strings
+    on_li = []
+    
+    for i in range(n):
+        a = input().split()
+        a.append(str(i))  # Append index to each string
+        on_li.append(a)  # Append index to each string
+    
+    # Sort based on the first two characters (as integers) and the last character
+    on_li.sort(key=lambda x: (int(x[0]), int(x[-1])))
+    
+    # Output the strings without the index part
+    for i in on_li:
+        print(i[0],i[1])  # Remove the index at the end before printing
 
 if __name__ == "__main__":
     main()
