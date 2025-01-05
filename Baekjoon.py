fbdf2c0d8fc0e9f7fcf45d4de9e11d5b0beb85bf
light <agy91521947@gmail.com>
2025-01-05 15:42:57 +0900

    30802 welcome kits

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,15 +1,19 @@
 def main():
-    data = []
     n = int(input())
-    for i in range(n):
-        a = input()
-        if a not in data:
-            data.append(a)
-
-    data.sort( key=lambda x : (len(x), x) )       # 데이터를 정렬합니다.
+    li = input().split()
+    t, p = map(int,input().split())
     
-    for i in data:
-        print(i)
-
+    t_or = 0
+    for i in range(len(li)): 
+        num = int(li[i])
+        if num ==0:
+            continue
+        else:
+            if num% t == 0:
+                t_or += num//t
+            else:
+                t_or += num//t + 1
+        
+    print(f"{t_or}\n{n//p} {n%p}")
 if __name__ == "__main__":
     main()
