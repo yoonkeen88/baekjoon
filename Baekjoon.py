120cccb536bccde790b5c0926f2527a0b11ff732
light <agy91521947@gmail.com>
2025-01-06 18:45:01 +0900

    1764_heard_saw

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,29 +1,23 @@
 import sys
 
 def main():
-    n = int(sys.stdin.readline().strip())
+    n, k = map(int, sys.stdin.readline().strip().split())
     se = set()
     full = set(map(str, range(1, 21)))  # 미리 전체 집합 생성
-
+    result = []
     for _ in range(n):
-        command = sys.stdin.readline().strip().split()
-        if command[0] == "all":
-            se = full.copy()
-        elif command[0] == "empty":
-            se = set()
-        else:
-            cmd, num = command[0], command[1]
-            if cmd == "add":
-                se.add(num)
-            elif cmd == "remove":
-                se.discard(num)
-            elif cmd == "check":
-                print(1 if num in se else 0)
-            elif cmd == "toggle":
-                if num in se:
-                    se.remove(num)
-                else:
-                    se.add(num)
+        name = sys.stdin.readline().strip()
+        se.add(name)
+
+    for _ in range(k):
+        name = sys.stdin.readline().strip()
+        if name in se:
+            result.append(name)
+    result.sort()
+    print(len(result))
+    print("\n".join(result))
+
+
 
 if __name__ == "__main__":
     main()
