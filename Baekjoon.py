6929b254ba659748f7ba0ba277a63f5165c1f4a4
light <agy91521947@gmail.com>
2025-01-06 19:22:01 +0900

    1003_fibonachi_counting_zero_one

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,13 +1,34 @@
 import sys
 
+def fibo_recursion(number: int) -> int:
+    """
+    Fibonacci function using recursion.
+    Tracks the number of times 0 and 1 are returned.
+    :param number: integer number
+    :return: integer number
+    """
+    global zero
+    global one
+
+    if number == 0:
+        zero += 1
+        return 0
+    elif number == 1:
+        one += 1
+        return 1
+    else:
+        return fibo_recursion(number - 1) + fibo_recursion(number - 2)
+
 def main():
+    global zero, one
+
     n = int(sys.stdin.readline().strip())
-    li = list(map(int, sys.stdin.readline().split()))
-    li.sort()
-    time = 0
-    for i in range(n):
-        for j in range(i+1):
-            time += li[j]
-    print(time)
+    for _ in range(n):
+        zero = 0
+        one = 0
+        number = int(sys.stdin.readline().strip())
+        fibo_recursion(number)
+        print(zero, one)
+
 if __name__ == "__main__":
     main()
