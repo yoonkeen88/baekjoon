236f7e246fe71fe24c75377342bf347b6c239fc0
light <agy91521947@gmail.com>
2025-01-06 16:45:47 +0900

    8958_score

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,20 +1,23 @@
-
-
 def main():
     n = int(input())  # Number of strings
-    on_li = []
-    
+    scores = []
     for i in range(n):
-        a = input().split()
-        a.append(str(i))  # Append index to each string
-        on_li.append(a)  # Append index to each string
-    
-    # Sort based on the first two characters (as integers) and the last character
-    on_li.sort(key=lambda x: (int(x[0]), int(x[-1])))
-    
-    # Output the strings without the index part
-    for i in on_li:
-        print(i[0],i[1])  # Remove the index at the end before printing
+        s = input()
+        scores.append(0)
+        score = 0
+        for j in range(len(s)):
+            if s[j] == 'X':
+                score = 0
+            elif s[j] == 'O':
+                if score == 0:
+                    score = 1
+                else:
+                    score +=1
+            scores[i] += score
+
+    for i in scores:
+        print(i)
+ 
 
 if __name__ == "__main__":
     main()
