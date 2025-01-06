a13e5dd5b2f708a95b68f22145923296e3b39aca
light <agy91521947@gmail.com>
2025-01-06 17:28:43 +0900

    1929 eratosthenes's_sieve

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,23 +1,19 @@
-def main():
-    n = int(input())  # Number of strings
-    scores = []
-    for i in range(n):
-        s = input()
-        scores.append(0)
-        score = 0
-        for j in range(len(s)):
-            if s[j] == 'X':
-                score = 0
-            elif s[j] == 'O':
-                if score == 0:
-                    score = 1
-                else:
-                    score +=1
-            scores[i] += score
+def sieve_of_eratosthenes(m, n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False 
 
-    for i in scores:
-        print(i)
- 
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    primes = [i for i in range(m, n + 1) if is_prime[i]]
+    return primes
+
+def main():
+    m,n = map(int, input().split())
+    prime =  sieve_of_eratosthenes(m,n)
+    for i in prime:
+        print(i)    
 
 if __name__ == "__main__":
     main()
