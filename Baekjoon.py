0961f123d10c64eeadde5dd345f5f4522793dc60
light <agy91521947@gmail.com>
2025-01-08 21:25:23 +0900

    iii

--- Baekjoon.py
+++ Baekjoon.py
@@ -2,20 +2,16 @@ import sys
 
 def main():
     # 입력 데이터 처리
-    data = sys.stdin.read().splitlines()
-    n = int(data[0])  # 첫 번째 줄은 개수
-    points = [tuple(map(int, line.split())) for line in data[1:]]  # 이후 줄은 (weight, height)
+    # n: 랜선의 갯수, k: 잘라야하는 횟수
 
-    result = [1] * n  # 모든 사람의 초기 등수는 1 (등수는 낮을수록 높음)
+    n,k = map(int, sys.stdin.readline().split())
+    data = list(sys.stdin.read().splitlines())
+    result =[]
+    for i in data:
+        for j in range(1,k-n+1):
+            result.append([int(i)//j,j])
+
     
-    # 모든 쌍을 비교
-    for i in range(n):
-        for j in range(n):
-            if i != j:  # 자기 자신과 비교 제외
-                if points[i][0] < points[j][0] and points[i][1] < points[j][1]:
-                    result[i] += 1  # i가 j보다 덩치가 작다면 등수 증가
-
-    # 결과 출력
     sys.stdout.write(" ".join(map(str, result)) + "\n")
 
 if __name__ == "__main__":
