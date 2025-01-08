c36985ae55edf61ada23cad94356ef8ca8e367f4
light <agy91521947@gmail.com>
2025-01-08 15:00:08 +0900

    10816_card_2

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,41 +1,21 @@
 import sys
-
-def fibo_recursion(number: int) -> int:
-    """
-    Fibonacci function using recursion.
-    Tracks the number of times 0 and 1 are returned.
-    :param number: integer number
-    :return: integer number
-    """
-    global zero
-    global one
-
-    if number == 0:
-        zero += 1
-        return 0
-    elif number == 1:
-        one += 1
-        return 1
-    else:
-        return fibo_recursion(number - 1) + fibo_recursion(number - 2)
+from collections import Counter
 
 def main():
-    global zero, one
+    data = sys.stdin.read().splitlines()
+    n = int(data[0])
+    card = list(map(int, data[1:n+2]))
+    m = int(data[n+2])
+    target = list(map(int, data[n+3].split()))
+    
+    # `card`의 각 숫자 빈도 계산
+    card_count = Counter(card)
+    
+    # 결과 계산
+    result = [card_count[i] for i in target]
 
-    n = int(sys.stdin.readline().strip())
-    for _ in range(n):
-        zero = 0
-        one = 0
-        number = int(sys.stdin.readline().strip())
-        fibo_recursion(number)
-        print(zero, one)
+    # 결과 출력
+    sys.stdout.write("\n".join(map(str, result)) + "\n")
 
 if __name__ == "__main__":
     main()
-    main1()
-
-
-
-
-
-
