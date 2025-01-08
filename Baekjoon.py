19f7138757cb558f7751634b2c7f070052e55216
light <agy91521947@gmail.com>
2025-01-08 16:54:09 +0900

    10828_stack

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,20 +1,49 @@
 import sys
-from collections import Counter
+
+class stack():
+    def __init__(self):
+        self.stack = []
+    
+    def push(self, value):
+        self.stack.append(value)
+        
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()
+        else:
+            return -1
+    def size(self):
+        return len(self.stack)
+    def empty(self):
+        return 0 if self.stack else 1
+    
+    def top(self):
+        return self.stack[-1] if self.stack else -1
+   
+
 
 def main():
-    data = sys.stdin.read().splitlines()
-    n = int(data[0])
-    card = list(map(int, data[1:n+2]))
-    m = int(data[n+2])
-    target = list(map(int, data[n+3].split()))
-    
-    # `card`의 각 숫자 빈도 계산
-    card_count = Counter(card)
-    
-    # 결과 계산
-    result = [card_count[i] for i in target]
+    n= int(sys.stdin.readline().strip())
+    result = []
+    st = stack()
+    for _ in range(n):
+        cmd = sys.stdin.readline().strip().split()
 
-    # 결과 출력
+        if "push" in cmd:
+            st.push(cmd[-1])
+        elif "pop" in cmd:
+            result.append(st.pop())
+
+        elif "size" in cmd:
+            result.append(st.size())
+
+        elif "empty" in cmd:
+            result.append(st.empty())
+
+        elif "top" in cmd:
+            result.append(st.top())
+        
+
     sys.stdout.write("\n".join(map(str, result)) + "\n")
 
 if __name__ == "__main__":
