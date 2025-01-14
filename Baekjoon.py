8c688a18c436b872f978f25a0e3dea41f9a19026
light <agy91521947@gmail.com>
2025-01-14 21:58:25 +0900

    11726_tile_insert

--- Baekjoon.py
+++ Baekjoon.py
@@ -1,14 +1,25 @@
-import sys
 def main():
-    h,m = map(int,sys.stdin.readline().strip().split())
-    m -= 45
-    if m <0:
-        if h-1 <0:
-            print(f'{24-1} {60 +m}')
-        else:
-            print(f"{h-1} {60+m}")
-    else:
-        print(print(f"{h} {m}"))
+    # 입력
+    n = int(input().strip())
     
+    if n == 0:  # n이 0일 경우 바로 출력
+        print(0)
+        return
+    
+    # dp 배열 초기화
+    dp = [0] * (max(3, n + 1))  # n이 2보다 작아도 dp[2]를 초기화하기 위해 크기를 최소 3으로 설정
+    dp[1] = 1
+    dp[2] = 2
+
+    division = 10007
+    if n > 2:
+        for i in range(3, n + 1):
+            dp[i] = (dp[i - 1] + dp[i - 2]) % division
+        print(dp[n])
+    elif n == 1:
+        print(dp[1])
+    elif n == 2:
+        print(dp[2])
+
 if __name__ == "__main__":
     main()
