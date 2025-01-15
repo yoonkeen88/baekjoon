0d5e51cadb587d3b16e2cb49fcc33dd7c8d0aca1
light <agy91521947@gmail.com>
2025-01-15 14:30:12 +0900

    cumulative_sum

--- new.py
+++ new.py
@@ -4,8 +4,7 @@ def main():
     # 입력
     n, m = map(int, input().strip().split())
     arr = list(map(int, sys.stdin.readline().strip().split()))
-    
-    # 누적 합 배열 생성
+  
     prefix_sum = [0] * (n+1)
     for i in range(1, n+1):
         prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1]
