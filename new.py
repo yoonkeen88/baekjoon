5b73a325d11553834b6c68ca57e6e1c5cc578b37
yoonkeen <agy91521947@gmail.com>
2025-01-17 18:47:12 +0900

    quizz

--- new.py
+++ new.py
@@ -1,19 +1,22 @@
-import sys
 
 def main():
     # 입력
     n, m = map(int, input().strip().split())
     arr = list(map(int, sys.stdin.readline().strip().split()))
-  
+    
+    # 누적 합 배열 생성
     prefix_sum = [0] * (n+1)
     for i in range(1, n+1):
         prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1]
     
     results = []
-    for _ in range(m):
-        i, j = map(int, input().strip().split())
-        results.append(prefix_sum[j] - prefix_sum[i - 1])
+    for quiz in data[n + 1:]:
+        if quiz.isdigit():
+            results.append(reverse_poket[int(quiz)])
+        else:
+            results.append(poket[quiz])
 
-    sys.stdout.write("\n".join(map(str, results)) + "\n")
+    sys.stdout.write("\n".join(map(str, results))+"\n")
+
 if __name__ == "__main__":
     main()
