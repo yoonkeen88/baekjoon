a2c04ac51106c562a7b5d922b29721043131aeab
light <agy91521947@gmail.com>
2025-01-18 13:10:46 +0900

    group_word

--- new.py
+++ new.py
@@ -1,19 +1,26 @@
 import sys
 
 def main():
-    # 입력
-    n, m = map(int, input().strip().split())
-    arr = list(map(int, sys.stdin.readline().strip().split()))
-  
-    prefix_sum = [0] * (n+1)
-    for i in range(1, n+1):
-        prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1]
+    # 입력 처리
+    input_data = sys.stdin.read().strip().split("\n")
+    n = int(input_data[0])  # 첫 번째 줄: 단어 개수
+    dat = input_data[1:]    # 이후 줄: 단어 리스트
     
-    results = []
-    for _ in range(m):
-        i, j = map(int, input().strip().split())
-        results.append(prefix_sum[j] - prefix_sum[i - 1])
+    cnt = 0  # 그룹 단어가 아닌 단어의 개수
+    
+    for word in dat:
+        seen = set()  # 등장한 문자 집합
+        prev_char = None  # 이전 문자
+        
+        for char in word:
+            if char != prev_char:
+                if char in seen:  # 이미 등장한 문자라면 그룹 단어가 아님
+                    cnt += 1
+                    break
+                seen.add(char)  # 등장한 문자 집합에 추가
+            prev_char = char  # 이전 문자 갱신
+    
+    print(n-cnt)
 
-    sys.stdout.write("\n".join(map(str, results)) + "\n")
 if __name__ == "__main__":
     main()
